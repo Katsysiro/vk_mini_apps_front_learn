b773ab653d4dbb5822da11c587bcdfbbb24d3f4f
210-P\210-P <210>
2022-03-17 20:30:28 +0500

    initial message brite bar

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,30 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, CardGrid, ContentCard, FixedLayout, Separator, WriteBar, WriteBarIcon } from '@vkontakte/vkui';
 
-const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
 
-		<Group header={<Header mode="secondary">Navigation Example</Header>}>
-			<Div>
-				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
-);
+const Home = ({ id, go, fetchedUser }) => {
+
+	const [text, setText] = useState("");
+
+	return (
+		<Panel id={id}>
+			<PanelHeader>Чатик</PanelHeader>
+
+			{/*fetchedUser &&
+			<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+				<Cell
+					before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+					description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+				>
+					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				</Cell>
+			</Group>*/}
+
+			{/*<Group header={<Header mode="secondary">Navigation Example</Header>}>
+				<Div>
+					<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
+						Show me the Persik, please
+					</Button>
+				</Div>
+			</Group>*/}
+
+			<Group>
+				<CardGrid size="l">
+					<ContentCard
+						subtitle="VKUI"
+						header="ContentCard example"
+						caption="VKUI Styleguide > Blocks > ContentCard"
+					/>
+					<ContentCard
+						onClick={() => {}}
+						src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
+						subtitle="unsplash"
+						header="brown and gray mountains under blue sky during daytime photo"
+						text="Mountain changji"
+						caption="Photo by Siyuan on Unsplash"
+						maxHeight={150}
+					/>
+				</CardGrid>
+			</Group>
+
+			<FixedLayout vertical="bottom">
+            	<Separator wide />
+				<WriteBar
+					after={
+						<Fragment>
+							{<WriteBarIcon 
+								mode="send" 
+								onClick={() => console.log('send')}
+								/>}
+						</Fragment>
+					}
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+					placeholder="Сообщение"
+				/>
+        	</FixedLayout>
+			
+		</Panel>
+	)
+};
 
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
